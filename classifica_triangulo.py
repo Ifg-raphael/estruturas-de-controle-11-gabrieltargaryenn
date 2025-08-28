3010f6e18d37b4dbafde38cdfe52b5e9aaaa6689
gabrieltargaryenn <gabrielalvestargaryen@gmail.com>
2025-08-28 18:55:32 -0300

    atividade finalizada

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,18 @@
 # Sua solução aqui
+
+# Leitura dos valores
+a = int(input('escreva um valor'))
+b = int(input('escreva um valor'))
+c = int(input('escreva um valor'))
+
+# Verifica se forma triângulo
+if a < b + c or b < a + c or c < a + b:
+    # Verifica o tipo de triângulo
+    if a == b == c:
+        print("equilátero")
+    elif a == b or b == c or a == c:
+        print("escaleno")
+    else:
+        print("isósceles")
+else:
+    print("Não forma triângulo")
